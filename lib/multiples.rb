0fde0e45ac393858fc38a01720b96c0720512618
Igor Veyner <igorveyner95@gmail.com>
2021-05-16 07:03:39 -0400

    finish multiples solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+def collect_multiples(limit) 
+  multiples = []
+
+  (3...limit).each do |i|
+    if i % 3 == 0 || i % 5 == 0
+      multiples << i
+    end
+  end
+
+  multiples
+end
+
+def sum_multiples(limit)
+  sum = 0 
+
+  (3...limit).each do |i|
+    sum += i if i % 3 == 0 || i % 5 == 0
+  end
+
+  sum
+end
